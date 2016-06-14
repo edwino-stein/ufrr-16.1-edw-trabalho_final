d2993a7205f6bcb0a6279efb736c7b702c1a9573
Edwino Stein <edwino.stein@gmail.com>
2016-06-13 23:41:05 -0400

    Adicionando suporte a edição de categorias

--- modules/Admin/Controller/Categorias.php
+++ modules/Admin/Controller/Categorias.php
@@ -15,7 +15,7 @@ class Categorias extends AbstractController{
         $error = null;
 
         if($this->app()->request()->hasPost('categoria-salvar')){
-            
+
             $categoria = new Categoria();
             $categoria->setNome($this->app()->request()->getPost('categoria-nome'));
             $categoria->setRemovido(false);
@@ -35,5 +35,43 @@ class Categorias extends AbstractController{
             'error' => $error,
             'categoria' => $categoria
         ));
+    }
+
+    public function updateAction(){
+
+        if(!$this->app()->request()->hasQuery('categoria_id')){
+            return 'Nenhuma categoria especificada';
+        }
+
+        $id = $this->app()->request()->getQuery('categoria_id');
+        $categoria = Categoria::findOneBy(array('id' => $id));
+        $error = null;
+
+        if($categoria === null){
+            return 'Nenhuma categoria especificada';
+        }
+
+
+        if($this->app()->request()->hasPost('categoria-salvar')){
+
+            $categoria->setNome($this->app()->request()->getPost('categoria-nome'));
+
+            if($categoria->save()){
+                return self::getView(
+                    array('message' => 'A categoria foi editada com sucesso.'),
+                    'categorias/operation-success.phtml'
+                );
+            }
+            else{
+                $error = 'Ocorreu algum erro durante a operação.';
+            }
+        }
+
+        return self::getView(array(
+            'error' => $error,
+            'categoria' => $categoria
+        ));
+
+
     }
 }
