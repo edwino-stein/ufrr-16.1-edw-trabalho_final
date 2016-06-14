e84e1d738d05371b9488da64d21f8b87045735b9
Edwino Stein <edwino.stein@gmail.com>
2016-06-13 23:01:25 -0400

    Adicionando suporte a edição de produtos

--- modules/Admin/Controller/Produtos.php
+++ modules/Admin/Controller/Produtos.php
@@ -51,6 +51,55 @@ class Produtos extends AbstractController{
         ));
     }
 
+    public function updateAction(){
+
+        if(!$this->app()->request()->hasQuery('produto_id')){
+            return 'Nenhum produto especificado';
+        }
+
+        $id = $this->app()->request()->getQuery('produto_id');
+        $produto = Produto::findOneBy(array('id' => $id));
+        $error = null;
+
+        if($produto === null){
+            return 'Nenhum produto especificado';
+        }
+
+        if($this->app()->request()->hasPost('produto-salvar')){
+
+            $produto->setNome($this->app()->request()->getPost('produto-nome'));
+            $produto->setValor($this->app()->request()->getPost('produto-valor'));
+            $produto->setCategoria($this->app()->request()->getPost('produto-categoria'));
+            $produto->setDescricao($this->app()->request()->getPost('produto-descricao'));
+
+            $thumbnail = $this->getFile('produto-imagem');
+            if($thumbnail !== null){
+                $produto->setThumbnail($thumbnail['blob']);
+                $produto->setThumbnailType($thumbnail['type']);
+            }
+            else if($this->app()->request()->hasPost('produto-imagem-reupdate')){
+                $produto->setThumbnail($this->app()->request()->getPost('produto-imagem-reupdate'));
+                $produto->setThumbnailType($this->app()->request()->getPost('produto-imagem-reupdate-type'));
+            }
+
+            if($produto->save()){
+                return self::getView(
+                    array('message' => 'O produto foi editado com sucesso.'),
+                    'produtos/operation-success.phtml'
+                );
+            }
+            else{
+                $error = 'Ocorreu algum erro durante a operação.';
+            }
+        }
+
+        return self::getView(array(
+            'categorias' => $this->getCategorias(),
+            'error' => $error,
+            'produto' => $produto
+        ));
+    }
+
     protected function getCategorias(){
         $categoriasModels = Categoria::fetchAll();
         $categorias = array();
