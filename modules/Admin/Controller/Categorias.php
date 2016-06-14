38da5316170147bed87f76a274f210c52d2bbfc7
Edwino Stein <edwino.stein@gmail.com>
2016-06-13 23:34:17 -0400

    Adicionando suporte a cadastro de categorias

--- modules/Admin/Controller/Categorias.php
+++ modules/Admin/Controller/Categorias.php
@@ -9,4 +9,31 @@ class Categorias extends AbstractController{
         $categorias = Categoria::findBy(array('removido' => false));
         return self::getView(array('categorias' => $categorias));
     }
+
+    public function createAction(){
+        $categoria = null;
+        $error = null;
+
+        if($this->app()->request()->hasPost('categoria-salvar')){
+            
+            $categoria = new Categoria();
+            $categoria->setNome($this->app()->request()->getPost('categoria-nome'));
+            $categoria->setRemovido(false);
+
+            if($categoria->save()){
+                return self::getView(
+                    array('message' => 'A categoria foi cadastrada com sucesso.'),
+                    'categorias/operation-success.phtml'
+                );
+            }
+            else{
+                $error = 'Ocorreu algum erro durante a operação.';
+            }
+        }
+
+        return self::getView(array(
+            'error' => $error,
+            'categoria' => $categoria
+        ));
+    }
 }
