3b7240e5ccb6d0786eb2f345ca12afbf6d43dd6a
Edwino Stein <edwino.stein@gmail.com>
2016-06-14 20:16:28 -0400

    Adicionando tela inicial da loja

---
+++ modules/Shop/Controller/Shop.php
@@ -0,0 +1,42 @@
+<?php
+namespace Shop\Controller;
+use Application\AbstractController;
+use Admin\Model\Categoria;
+use Shop\Model\ProdutoView;
+use Shop\Model\ProdutoThumbnail;
+
+class Shop extends AbstractController{
+
+    public function indexAction(){
+
+        $categorias = Categoria::findBy(array('removido' => false), array('offset' => 0, 'limit' => 5));
+        $produtos = array();
+        foreach ($categorias as $key => $c) {
+            $produtos[$key] = ProdutoView::findBy(
+                array('removido' => false, 'categoria' => $c->getId()),
+                array('offset' => 0, 'limit' => 4)
+            );
+        }
+
+        return self::getView(array('categorias' => $categorias, 'produtos' => $produtos));
+    }
+
+    public function thumbnailAction(){
+
+        $this->template = null;
+        $produtoId = $this->app()->request()->getQuery('produto_id', null);
+        $produto = ProdutoThumbnail::findOneBy(array('id' => $produtoId));
+
+        if($produto == null || empty($produto->getThumbnail())){
+            $public = $this->app()->getDir('public');
+            $noThumbnail = $public.'/img/sem-foto.png';
+            header('Content-Type: '.mime_content_type($noThumbnail));
+            return file_get_contents($noThumbnail);
+        }
+        else {
+            header('Content-Type: '.$produto->getThumbnailType());
+            return base64_decode($produto->getThumbnail());
+        }
+    }
+
+}
--- modules/Shop/Shop.php
+++ modules/Shop/Shop.php
@@ -4,13 +4,18 @@ use Application\Application;
 
 class Shop extends Application{
     protected $config = array(
-        'db' => array(),
+        'db' => array(
+            'driver' => 'mysql',
+            'dbname' => 'web_final1',
+            'user' => 'web_final1',
+            'password' => '123456'
+        ),
         'view' => array(
             'charset' => 'UTF-8',
             'contentName' => 'content',
             'noTemplate' => false,
             'defaultTemplate' => 'template.phtml'
         ),
-        'defaultController' => 'home'
+        'defaultController' => 'shop'
     );
 }
